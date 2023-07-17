fb8df02d672b54a2309fc39d02abb87868de7322
alessandrostorchi92 <storchi.alle@gmail.com>
2023-07-17 16:05:16 +0200

    Esercizio Completato

--- js/main.js
+++ js/main.js
@@ -2,90 +2,70 @@
 // Ogni secondo il nostro countdown dovrà scalare fino alle 9:30 di domani mattina!
 
 // Procedimento: 
-// 1) Imposto la data di fine del conto alla rovescia mediante la funzione new Date(...).getTime(), che mi consente di impostare una data e ora specifica
-// 2) Assegno ad una variabile la data e l'ora corrente. Uso come variabile let affinchè la data possa aggiornarsi all'interno della funzione
-// 3) Recupero la data attuale all'avvio della pagina
-// 4) Verifico la corretta impostazione della data corrente
-// 5)  Verifico la differenza tra le due date in millisecondi
-// 6) Imposto i calcoli della differenza tra le date per ottenere la differenza in giorni, ore, minuti e secondi 
-// Creo la funzione per settare l'intervallo di tempo che sarà di un secondo
+
+// 1) Assegno ad una variabile la data si inzio della lezione e ad un'altra la data e l'ora attuale
+// 2) Calcolo la loro differenza in millisecondi
+// 3) Converto la differenza in giorni, ore, minuti e secondi
+// 4) Verifico la differenza del passaggio precedente
+// 5)
+
 
 "use strict"
 
-// Assegno ad una variabile l'HTMLElement del Countdown
-const countdown = document.getElementById("countdown");
+// Fisso una variabile che sta ad indicare quando inizierà la lezione
 
-// imposto una data e ora specifica
+const lessonTime = new Date("Jul 18, 2023 09:30:00").getTime();
 
-const lessonStarted = new Date("Jul 18, 2023 09:30:00").getTime();
+// Fisso una variabile che sta ad indicare la data e l'ora corrente. Uso come variabile let affinchè la data possa aggiornarsi successivamente all'interno della funzione
 
-// Assegno ad una variabile la data e l'ora corrente. Uso come variabile let affinchè la data possa aggiornarsi all'interno della funzione
-let now = new Date();
+let now = new Date().getTime();
 
-// Recupero la data attuale all'avvio della pagina
-countdown .innerHTML = now.getHours() + ":" + now.getMinutes() + ":" + now.getSeconds();
+// Questa è la funzione che aggiorna il timer visualizzato sul documento html senza dover ricaricare la pagina html per vedere l'aggiornamento (N.B. Impostando un timer più breve risolvo il problema dell'asincronicità dovuta ai rallentamenti del browser, quindi posso impostare anche 500, ecc..)
 
-const timerCountdown = setInterval(updateCountdown, 1000);
+// Invocazione della funzione
 
-// setInterval(updateCountdown, 1000); Impostando un timer più breve risolvo il problema dell'asincronicità dovuta ai rallentamenti del browser, quindi posso impostare 500 ecc...
+const updateCountdown = setInterval(countdownTimer, 1000);
 
-// Imposto una funzione che ogni secondo aggiorna il mio countdown senza dover ricaricare la pagina html
-function updateCountdown () {
-    now = new Date();
-    
-    countdown .innerHTML = now.getHours() + ":" + now.getMinutes() + ":" + now.getSeconds();
-    
-    // Verifico che il countdown stia funzionando 
-    console.log("countdown update");
-} 1000;
+// Dichiarazione della funzione
 
+function countdownTimer() {
 
+    now = new Date().getTime();
 
+    const remainingTime = lessonTime - now;
 
+    /* Converto questa differenza da millisecondi in giorni, ore, minuti e secondi */
 
+    const days = Math.floor(remainingTime / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((remainingTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((remainingTime % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((remainingTime % (1000 * 60)) / 1000);
 
+    /* Verifico questa differenza da millisecondi in giorni, ore, minuti e secondi */
 
+    console.log(days)
+    console.log(hours)
+    console.log(minutes)
+    console.log(seconds)
 
+    // Modifico il cronometro dell'HTML in tempo reale
+    document.getElementById("countdown-timer").innerHTML = days + "gg " + hours + "ore " + minutes + "min " + seconds + "sec";
 
+    if (remainingTime < 0) {
+        clearInterval(updateCountdown);
+        document.getElementById("countdown").innerHTML = "La lezione di Boolean è iniziata!";
+    }
 
-
-
-
-
-
-
-
-
-
-
-// // Assegno ad una variabile la data di fine del conto alla rovescia
-// const lessonIsBegun = new Date("July 15, 2023 09:30:00").getTime();
-
-// // Verifico data di fine del conto alla rovescia
-// console.log(lessonIsBegun);
+};
 
 
 
-// // Verifico data di fine del conto alla rovescia
-// console.log(now);
 
-// // Calcolo la differenza tra la data di oggi e quella impostata in millisecondi
-// const difference = lessonIsBegun - now;
 
-// // Verifico la differenza tra le due date in millisecondi
-// console.log(difference);
 
-// // Imposto i calcoli della differenza tra le date per ottenere la differenza in giorni, ore, minuti e secondi 
-// const days = Math.floor(difference / (1000 * 60 * 60 * 24));
-//     const hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-//     const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
-//     const seconds = Math.floor((difference % (1000 * 60)) / 1000);
 
-// // Verifico la differenza tra le due date per ottenerla in giorni, ore, minuti e secondi
-//     console.log(days)
-//     console.log(hours)
-//     console.log(minutes)
-//     console.log(seconds)
+
+
 
 // // Dichiaro la costante per settare l'intervallo di tempo che sarà di un secondo
 
